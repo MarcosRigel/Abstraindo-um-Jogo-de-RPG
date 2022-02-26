3769f9a4d2d15daac7b2ef8071402111128ad88b
Marcos Rigel <marcosrigel99@gmail.com>
2022-02-25 21:27:09 -0300

    Primeiros passos para pensar orientado a objetos

---
+++ Jogo-de-RPG/src/Entities/Arus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_de_RPG.src.Entities;
+{
+    class Arus
+    {
+    }
+}
