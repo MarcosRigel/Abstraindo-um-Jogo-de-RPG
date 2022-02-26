cd300e2b4f44bb3b12c9a699568eca0e61e924ef
Marcos Rigel <marcosrigel99@gmail.com>
2022-02-26 18:26:58 -0300

    Criando o metoo construtor o heroi

--- Jogo-de-RPG/Program.cs
+++ Jogo-de-RPG/Program.cs
@@ -7,10 +7,7 @@ namespace Jogo_de_RPG
     {
         static void Main(string[] args)
         {
-            Arus hero = new Arus();
-            hero.Name = "Arus";
-            hero.level = 2;
-            hero.HeroType = "Knight";
+            Arus hero = new Arus("Arus", 23, "Knight");
 
             Console.Write("");
         }
--- Jogo-de-RPG/src/Entities/Arus.cs
+++ Jogo-de-RPG/src/Entities/Arus.cs
@@ -8,6 +8,18 @@ namespace Jogo_de_RPG.src.Entities
 {
     class Arus
     {
+        public Arus(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public Arus()
+        {
+
+        }
+
         public string Name;
         public int level;
         public string HeroType;
